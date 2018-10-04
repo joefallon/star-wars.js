b6a68ab90bc4bd701b043a17337533b9b4de4e43
Joe Fallon <joseph.t.fallon@gmail.com>
2018-10-03 20:27:40 -0700

    The gateways were added to the species model constructor.

--- src/models/SpeciesModel.ts
+++ src/models/SpeciesModel.ts
@@ -1,3 +1,9 @@
-export class SpeciesModel {
+import { Gateways } from '../gateways/Gateways';
 
+export class SpeciesModel {
+    private _gateways: Gateways;
+
+    public constructor(gateways: Gateways) {
+        this._gateways = gateways;
+    }
 }
--- src/models/SpeciesModelFactory.ts
+++ src/models/SpeciesModelFactory.ts
@@ -1,8 +1,12 @@
 import { SpeciesModel } from './SpeciesModel';
+import { GatewaysFactory } from '../gateways/GatewaysFactory';
 
 export class SpeciesModelFactory {
 
     public static create(): SpeciesModel {
-        return new SpeciesModel();
+        const gateways = GatewaysFactory.create();
+        const model    = new SpeciesModel(gateways);
+
+        return model;
     }
 }
